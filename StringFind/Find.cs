42f640d80fcd03ff0630a5195db2dadf6e4cfbc6
FatherRetart <berezin.evgenij.01@mail.ru>
2021-11-08 21:48:27 +0500

    Добавьте файлы проекта.

---
+++ StringFind/Find.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFind
+{
+	class Find
+	{
+		public static bool Contains(string str, string findStr)
+		{
+			int i, j;
+			int[] next = new int[findStr.Length]; // Префикс-массив
+			for (next[0] = j = -1, i = 1; i < findStr.Length; i++) // Цикл заполенния префикс-массива
+			{
+				for (; j > -1 && findStr[j + 1] != str[i]; j = next[j]) ;
+				if (findStr[j + 1] == findStr[i]) j++;
+				next[i] = j;
+			}
+
+			for (j = -1, i = 0; i < str.Length; i++) // Цикл нахождения подстроки
+			{
+				for (; j > -1 && findStr[j + 1] != str[i]; j = next[j]) ;
+				if (findStr[j + 1] == str[i]) j++;
+				if (j == findStr.Length-1) return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ StringFind/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringFind
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string str = "123312";
+			Console.WriteLine(Find.Contains(str,"3122"));
+		}
+	}
+}
